f8ea6a7dacc3a57d7912db0cd1410a30740c56e2
Lucas Plug <78904725+lucasplug@users.noreply.github.com>
2026-07-09 19:39:54 +0200

    Add env_bool function and update FEED_URL
    Added a function to parse boolean environment variables and updated FEED_URL.

--- config.py
+++ config.py
@@ -1,10 +1,23 @@
 import os
 
-FEED_URL = os.getenv("FEED_URL", "https://zwaailicht.nu/feed/meldingen/katwijk.xml")
+def env_bool(name: str, default: bool = False) -> bool:
+    return os.getenv(name, str(default)).lower() in ("true", "1", "yes", "on")
+
+FEED_URL = os.getenv("FEED_URL", "https://alarmeringen.nl/feeds/city/katwijk.rss")
+
 MQTT_HOST = os.getenv("MQTT_HOST", "localhost")
 MQTT_PORT = int(os.getenv("MQTT_PORT", "1883"))
 MQTT_USER = os.getenv("MQTT_USER", "")
 MQTT_PASSWORD = os.getenv("MQTT_PASSWORD", "")
 MQTT_TOPIC_BASE = os.getenv("MQTT_TOPIC_BASE", "alarmeringen/katwijk")
+
 INTERVAL = int(os.getenv("INTERVAL", "60"))
 HISTORY_SIZE = int(os.getenv("HISTORY_SIZE", "5"))
+
+GEOCODING_ENABLED = env_bool("GEOCODING_ENABLED", True)
+HOME_LAT = os.getenv("HOME_LAT")
+HOME_LON = os.getenv("HOME_LON")
+GEOCODER_USER_AGENT = os.getenv(
+    "GEOCODER_USER_AGENT",
+    "alarmeringen-katwijk/1.0"
+)
